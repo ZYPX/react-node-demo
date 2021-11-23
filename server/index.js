8ca60a1be5ba09920ac12378fdf3d679222a296c
ZYPX <zachsnkrs@gmail.com>
2021-11-23 15:18:27 -0500

    fixed index paths

--- server/index.js
+++ server/index.js
@@ -64,11 +64,11 @@ app.get("/api", (req, res) => {
 
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../../client/build')));
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../../client/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
